29405faeaa1b0d4f2bc6207b0590920048b528f5
Abdel-RahmanSaied <abdo.am169@gmail.com>
2025-08-08 03:49:09 +0300

    update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { SignupComponent } from './features/auth/signup/signup.component'
 import { TripDetailsComponent } from './features/auth/trip-details/trip-details.component'
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/landing', pathMatch: 'full' },
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
   { path: 'landing', component: LandingComponent },
   { path: 'signup', component: SignupComponent},
   { path: 'trip-details', component: TripDetailsComponent}
